0a46e10a75bfbecc831af572523655eb4e4bec51
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-02-06 22:18:44 +0100

    update changes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // IMPORT PACKAGE
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-// ROUTING (HTTP METHOD, URL)
-// app.get('/', (req, res) => {
-//     res.status(200).send('Hi this is the app')
-// })
+// MIDDLEWARE
+app.use(express.json());
+app.use((req, res, next) => {
+  req.requestedAT = new Date().toISOString();
+  next();
+});
+app.use(morgan('dev'))
 
 // GET: /api/v1/movies
 const fs = require("fs");
@@ -13,6 +17,7 @@ const movies = JSON.parse(fs.readFileSync("./data/movies.json", "utf-8"));
 app.get("/api/v1/movies", (req, res) => {
   res.status(200).json({
     status: "success",
+    requestedAT: req.requestedAT,
     count: movies.length,
     data: {
       movies: movies,
@@ -33,17 +38,17 @@ app.get("/api/v1/movies/:id", (req, res) => {
     });
   }
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      movie: movie,
-    },
+  fs.readFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    res.status(200).json({
+      status: "success",
+      data: {
+        movie: movie,
+      },
+    });
   });
 });
 
 // POST: /api/v1/movies
-app.use(express.json());
-
 app.post("/api/v1/movies", (req, res) => {
   const newId = movies.length + 1;
   const newMovie = Object.assign({ id: newId }, req.body);
@@ -55,6 +60,57 @@ app.post("/api/v1/movies", (req, res) => {
       status: "success",
       data: {
         movie: newMovie,
+      },
+    });
+  });
+});
+
+// PATCH: /api/v1/movies/:id
+app.patch("/api/v1/movies/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const movieToUpdate = movies.find((mov) => mov.id === id);
+  const index = movies.indexOf(movieToUpdate); // eg: movie with id = 4, index = 3
+
+  Object.assign(movieToUpdate, req.body);
+
+  movies[index] = movieToUpdate;
+
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    if (!movieToUpdate) {
+      res.status(404).json({
+        status: "fail",
+        message: "movie not found (id doesn't exist)",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        movie: movieToUpdate,
+      },
+    });
+  });
+});
+
+// DELETE: /api/v1/movies/:id
+app.delete("/api/v1/movies/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const movieToDelete = movies.find((mov) => mov.id === id);
+  const index = movies.indexOf(movieToDelete);
+
+  if (!movieToDelete) {
+    return res.status(404).json({
+      status: "fail",
+      message: "movie not found (id doesn't exist)",
+    });
+  }
+
+  movies.splice(index, 1);
+
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    res.status(204).json({
+      status: "success",
+      data: {
+        movie: null,
       },
     });
   });

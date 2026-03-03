d84f10d52c785b1a92fe3422b78412591a49dec4
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-03-03 15:58:02 +0100

    u

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,18 @@
 // IMPORT PACKAGES
 const User = require("../models/userModel");
 const asyncErrorHandler = require("../utils/asyncErrorHandler");
+const jwt = require('jsonwebtoken')
 
 const signup = asyncErrorHandler(async (req, res) => {
     const newUser = await User.create(req.body)
 
+    const token = jwt.sign({id: newUser._id}, process.env.SECRET_STR, {
+        expiresIn: process.env.EXPIRED_IN
+    })
+
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
